f90210ab3d44ce79fe731555c79842ede646ae94
ChanikyaMohan <konyalachanikya@gmail.com>
2017-01-03 15:38:14 -0500

    added dispatcher for creatingtodo list action

--- Flux/src/js/stores/TodoStore.js
+++ Flux/src/js/stores/TodoStore.js
@@ -1,4 +1,5 @@
 import { EventEmitter } from "events";
+import dispatcher from "../dispatcher"
 
 class TodoStore extends EventEmitter {
     constructor() {
@@ -31,8 +32,18 @@ class TodoStore extends EventEmitter {
     getAll(){
         return this.todos;
     }
+
+    handleActions(action){
+        switch (action.type){
+            case "CREATE_TODO" : {
+                this.createTodo(action.text);
+            }
+        }
+    }
 }
 
 const todoStore = new TodoStore;
 window.todoStore = todoStore;
+dispatcher.register(todoStore.handleActions.bind(todoStore));
+window.dispatcher = dispatcher;
 export default todoStore;
